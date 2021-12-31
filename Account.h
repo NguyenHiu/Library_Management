65dd6aee121136209131261680c09e626018e258
Nguyen Hieu <87634727+NguyenHiu@users.noreply.github.com>
2022-01-01 01:22:11 +0700

    update Account

--- Account.h
+++ Account.h
@@ -16,18 +16,42 @@ enum AccountStatus
 class Account
 {
 private:
-    std::string aID, aPassword;
+    std::string aID, aUsername, aPassword;
+    Person aInfo;
+    std::vector<std::string> aBooksBorrowed;
     AccountStatus aStatus;
-    //Person aInfo;
-
 public:
-    Account() { };
-    Account(std::string us, std::string pw)
-    {
-        aID = us; aPassword = pw;
-        aStatus = Active;
-    }
-    virtual ~Account() {};
-    
+    Account();
+    Account(std::string ID, std::string US, std::string Pass, Person info);
+    bool checkLogOut(std::string US, std::string Pass);
+    bool changePass(std::string Pass);
+    bool resetPass();
+    bool updateProfile(vector<string> profile);
+    bool blockAccount();
+    void updateBooksBorrowed(std::vector<std::string> list);
+    std::vector<std::string> displayBorrowedBooks();
+    string toString() const;
 };
 
+Account::Account()
+{
+    aID = "";
+    aUsername = "";
+    aPassword = "";
+    aInfo = Person();
+    aStatus = Active;
+}
+
+Account::Account(std::string ID, std::string US, std::string Pass, Person info)
+{
+    aID = ID;
+    aUsername = US;
+    aPassword = Pass;
+    aInfo = info;
+    aStatus = Active;
+}
+
+bool Account::checkLogOut(std::string US, std::string Pass)
+{
+    
+}
